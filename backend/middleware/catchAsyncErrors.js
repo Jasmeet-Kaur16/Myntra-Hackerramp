e7520949683ee54cf978237319157db814678a16
Jasmeet-Kaur16 <92389678+Jasmeet-Kaur16@users.noreply.github.com>
2024-07-15 21:54:32 +0530

    Delete backend directory

--- backend/middleware/catchAsyncErrors.js
+++
@@ -1,7 +0,0 @@
-module.exports = func => (req, res, next) => Promise.resolve(func(req, res, next)).catch((error) => {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  });
--- backend/routes/trends.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-const {trend} = require('../controllers/trends')
-
-router.get("/", trend);
-
-module.exports = router;
